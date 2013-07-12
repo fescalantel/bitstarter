903d0ac5213c842bacb462dd7f5af80f285fac60
Ubuntu <fabian_escalante@hotmail.com>
2013-07-12 17:47:14 +0000

    xx

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs=require('fs');
 
 var app = express.createServer(express.logger());
 
-var fichero=fs.readFileSync('index.html');
+var fichero=fs.readFileSync('index.html').toString();
 
 
 app.get('/', function(request, response) {
