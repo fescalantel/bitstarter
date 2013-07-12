b4e251112e2de76d6350715e48253437cca95ac4
Ubuntu <fabian_escalante@hotmail.com>
2013-07-12 17:20:46 +0000

    Fichero

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs=require('fs');
 
 var app = express.createServer(express.logger());
 
+var fichero=fs.readFile('index.html');
+
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+  response.send(fichero);
 });
 
 var port = process.env.PORT || 5000;
